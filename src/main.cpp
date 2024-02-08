8d8aba64c25b708730471c4e152ce584d64c17e3
Roboticsnerd11 <qlobwork@gmail.com>
2024-02-08 08:33:57 -0500

    right stuff

--- src/main.cpp
+++ src/main.cpp
@@ -8,37 +8,31 @@
 #include <cmath>
 #define M_PI 3.14159265358979323846
 
-uint32_t pidStartTime;
-
 
 pros::Motor leftBottom(12, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_ROTATIONS); //middle
-pros::Motor leftBack(13, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS); // Make left side stronger
-pros::Motor leftTop(2, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS); //front
+pros::Motor leftBack(13, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_ROTATIONS); // Make left side stronger
+pros::Motor leftTop(2, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_ROTATIONS); //front
 pros::Motor rightBottom(19, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor rightBack(17, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor rightTop(9, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor rightBack(17, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor rightTop(10, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
 
 
 pros::Motor_Group leftDrive({leftBottom, leftBack, leftTop});
 pros::Motor_Group rightDrive({rightBottom, rightBack, rightTop});
 
 
-pros::Motor Intake(10);
-pros::Rotation Rotationsensor(19);
+pros::Motor Intake(8);
+pros::Rotation Rotationsensor(14);
 pros::Motor catapultMotor(18, pros::E_MOTOR_GEARSET_18, true,
                         pros::E_MOTOR_ENCODER_DEGREES);
 pros::Controller MasterController(pros::E_CONTROLLER_MASTER);
-pros::ADIDigitalOut RW('A', false);
-pros::ADIDigitalOut LW('H', false);
-pros::Imu imu_sensor(17);
-pros::Rotation PIDRotationSensor(50); 
+pros::ADIDigitalOut RW('C', false);
+pros::ADIDigitalOut LW('A', false);
+pros::Imu imu_sensor(11);
+//pros::Rotation PIDRotationSensor(50); 
 pros::ADIDigitalOut blockerUp('B', false);
-pros::ADIDigitalOut blockerDown('C', false);
+pros::ADIDigitalOut blockerDown('D', false);
 
-
-
-//bool variableA = true;
-//bool variableB = true;
 bool wingsOpenLeft = false;
 bool rightWingsOpen = false;
 bool leftstate = true;
@@ -53,73 +47,27 @@ bool catacurrent = true;
 bool catacontrol;
 bool prevPressed = false;
 const int REST_POSITION = 25700;
-const int DESIRED_SHOTS = 50;
 bool spam = false;
-
-
-
-void displayStuff() { //Displays things
-
- bool leftBottomConnected = leftBottom.get_position() != 0;
-    pros::lcd::print(1, "Left Bottom Connected: %s", leftBottomConnected ? "Yes" : "No");
-
-    bool leftBackConnected = leftBack.get_position() != 0;
-    pros::lcd::print(2, "Left Back Connected: %s", leftBackConnected ? "Yes" : "No");
-
-    bool leftTopConnected = leftTop.get_position() != 0;
-    pros::lcd::print(3, "Left Top Connected: %s", leftTopConnected ? "Yes" : "No");
-
-    bool rightTopConnected = rightTop.get_position() != 0;
-    pros::lcd::print(4, "Right Top Connected: %s", rightTopConnected ? "Yes" : "No");
-    
-    bool rightBottomConnected = rightBottom.get_position() != 0;
-    pros::lcd::print(5, "Right Bottom Connected: %s", rightBottomConnected ? "Yes" : "No");
-    
-    bool rightBackConnected = rightBack.get_position() != 0;
-    pros::lcd::print(6, "Right Back Connected: %s", rightTopConnected ? "Yes" : "No");
-    
-    pros::lcd::print(7, "Left Bottom Temp: %d", leftBottom.get_temperature());
-    pros::lcd::print(8, "Left Back Temp: %d", leftBack.get_temperature());
-    pros::lcd::print(9, "Left Top Temp: %d", leftTop.get_temperature());
-    pros::lcd::print(10, "Right Bottom Temp: %d", rightBottom.get_temperature());
-    pros::lcd::print(11, "Right Back Temp: %d", rightBack.get_temperature());
-    pros::lcd::print(12, "Right Top Temp: %d", rightTop.get_temperature());
-
-    pros::delay(1000);
-
-}
-
 
 // PID constants
 double KP = 7.5;
 double KD = 1.4;
 
-
 // turn PID variables
 double turnKP = 1.0;
 double turnKD = 0.5;
-
 
 // Error in position for drive straight PID
 double error, prevError, errorRate, velocity;
 
 
-void resetTimer() {
-    pidStartTime = pros::millis();
-}
-
-bool isTimeLimitExceeded(int timeLimit) {
-    return (pros::millis() - pidStartTime) > timeLimit;
+double getPosition() 
+{
+  return 2.75*M_PI*leftBottom.get_position()*(36.0/48.0);
 }
 
 
-
-double getPosition() {
-  return 3.25*M_PI*leftBottom.get_position()*(3.0/5.0);
-}
-
-
-void drivePID(double distance, int timeLimit = 6000) {
+void drivePID(double distance) {
     // reset motors
     leftDrive.tare_position();
     rightDrive.tare_position();
@@ -128,15 +76,12 @@ void drivePID(double distance, int timeLimit = 6000) {
     error = distance;
     prevError = error;
   
-    resetTimer();
 
     // Continue the loop while drive PID is enabled
-    while (std::abs(error) > 0.5 && !isTimeLimitExceeded(timeLimit)) {
+    while (std::abs(error) > 0.5) {
         // Calculate average position and its derivative for drive straight PID
         error = distance - getPosition();
         errorRate = error - prevError;
-
-
 
 
         velocity = KP * error + KD * errorRate;
@@ -157,9 +102,6 @@ void drivePID(double distance, int timeLimit = 6000) {
 
         prevError = error;
 
-       if (isTimeLimitExceeded(timeLimit)) {
-            break;  // Break out of the loop if the time limit is exceeded
-        }
 
 
         pros::delay(20);
@@ -171,19 +113,16 @@ void drivePID(double distance, int timeLimit = 6000) {
 }
 
 
-void turnPID(double degrees, double scaling = 1.0, int timeLimit = 6000) { //, double timeout = -1) {
+void turnPID(double degrees, double scaling = 1.0) { //, double timeout = -1) {
     imu_sensor.tare_heading();
     error = degrees;
     prevError = error;
 
-
     double startTime = pros::millis();
 
     double averageHeading, heading1;
-    
-    resetTimer();
-    
-    while (std::abs(error) > 8 || leftBack.get_actual_velocity() > 15 || rightBack.get_actual_velocity() > 15 && !isTimeLimitExceeded(timeLimit)) {
+
+    while (std::abs(error) > 8 || leftBack.get_actual_velocity() > 15 || rightBack.get_actual_velocity() > 15) {
         // distance subtracted by the average of the four ground motors
         heading1 = imu_sensor.get_heading();
         if (heading1 > 180) heading1 -= 360;
@@ -206,17 +145,9 @@ void turnPID(double degrees, double scaling = 1.0, int timeLimit = 6000) { //, d
             leftDrive = -velocity*scaling;
             rightDrive = velocity*scaling;
         }
-            //if (timeout != -1) {
-            //if (pros::millis() > (startTime + timeout)) {
-                //break;
-                //}
-            //}
+          
         prevError = error;
-        
-        if (isTimeLimitExceeded(timeLimit)) {
-            break;  // Break out of the loop if the time limit is exceeded
-        }
-        
+      
         
         pros::delay(20);
     }
@@ -227,59 +158,43 @@ void turnPID(double degrees, double scaling = 1.0, int timeLimit = 6000) { //, d
 
 void autonomous()
 {
-/*displayStuff();
+ 
+ 
 
- leftDrive.move(127);
- rightDrive.move(127);
+ leftDrive.move(-127);
+ rightDrive.move(-127);
 
     
- pros::delay(1000); //might not be able to hard code cause unpredicable 
+ pros::delay(495); //might not be able to hard code cause unpredicable 
 
  leftDrive.move(0);
  rightDrive.move(0);
 
-resetTimer();
-drivePID(-7);
-resetTimer();
-turnPID(45, 1); 
-resetTimer();
-drivePID(-5);
-
-
-int shotsFired = 0;
-for (int i = 0; i < DESIRED_SHOTS; ++i) {
-        // Fire the catapult
-        catapultMotor.move(0.7 * 127);
-        pros::delay(500);  
-        
-        // Stop the catapult
-        catapultMotor.move(0);
-
-    
-        shotsFired++;
-    }
-
- if (shotsFired >= DESIRED_SHOTS) {
-       
-        catapultMotor.move_absolute(REST_POSITION, 127);
-            
-        pros::delay(1000);  // Adjust this delay based on the time it takes for the catapult to reset
-        
-        // Stop the catapult
-        catapultMotor.move(0);
-    }
-
-resetTimer();
+pros::delay(2000);
+drivePID(10);
+pros::delay(2000);
+turnPID(80, 1); 
 drivePID(8);
-resetTimer();
+
+catapultMotor.move(127);
+pros::delay(4000);
+
+
+
+
+
+
+
+drivePID(8);
+
 turnPID(90, 1);
-resetTimer();
+
 drivePID(5);
-resetTimer();
+
 turnPID(-25, 1);
-resetTimer();
+
 drivePID(37);
-resetTimer();
+
 turnPID(-10, 1);
 
 
@@ -293,7 +208,7 @@ pros::delay(1000); //might not be able to hard code cause unpredicable
 
 leftDrive.move(0);
 rightDrive.move(0);
-resetTimer();
+
 drivePID(-10);
 
 
@@ -305,21 +220,20 @@ pros::delay(300);
 leftDrive.move(0);
 rightDrive.move(0);
 
-resetTimer();
 drivePID(-10);
-resetTimer();
+
 turnPID(-98, 1);
 
 LW.set_value(false);
 RW.set_value(false);
 
-resetTimer();
+
 drivePID(20);
-resetTimer();
+
 turnPID(40, 1);
-resetTimer();
+
 drivePID(5);
-resetTimer();
+
 turnPID(45, 1);
 
 LW.set_value(true);
@@ -341,11 +255,12 @@ rightDrive.move(127);
 pros::delay(300);
 
 leftDrive.move(0);
-rightDrive.move(0);*/
+rightDrive.move(0);
 
 
-///////////////////////////////////////////////////Auton 2 | close side /////////////////////////////////////////////////////////////
-/*drivePID(27);
+///////////////////////////////////////////////////Auton 2 | close side ///////////////////////////////////////////////////////////// 
+/*
+drivePID(27);
 turnPID(50, 1);
 LW.set_value(true);
 RW.set_value(true);
@@ -366,7 +281,7 @@ drivePID(-5);
 turnPID(-30, 1);
 
 
-*/
+
 //////////////////////////////////////////////////Auton 3 far side)
   drivePID(-8);
   Intake.move(-127);
@@ -400,11 +315,8 @@ turnPID(-30, 1);
   turnPID(-90, 1);
   drivePID(-30);
 
-
+*/
 }
-
-
-
 
 
 void opcontrol()
@@ -415,38 +327,35 @@ void opcontrol()
     while (true)
     {
 
-   displayStuff();
-   
-      
-       
         drivePower = MasterController.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
         turnPower = MasterController.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
         leftstate = MasterController.get_digital(pros::E_CONTROLLER_DIGITAL_R2);
         rightstate = MasterController.get_digital(pros::E_CONTROLLER_DIGITAL_L2);
         catacurrent = MasterController.get_digital(pros::E_CONTROLLER_DIGITAL_L1);
         catacontrol = MasterController.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
-        leftBottom.move(1.5 * (drivePower + turnPower));
-        leftBack.move(1.5 * (drivePower + turnPower));
-        leftTop.move(1.5 * (drivePower + turnPower));
+        leftBottom.move(1 * (drivePower + turnPower));
+        leftBack.move(1 * (drivePower + turnPower));
+        leftTop.move(1 * (drivePower + turnPower));
         rightBottom.move(drivePower - turnPower);
         rightTop.move(drivePower - turnPower);
         rightBack.move(drivePower - turnPower);
 
-        if (MasterController.get_digital(pros::E_CONTROLLER_DIGITAL_A)) {
+       /* if (MasterController.get_digital(pros::E_CONTROLLER_DIGITAL_A)) { //blocker up
             // Blocker up
             blockerUp.set_value(true);
             blockerDown.set_value(false);
-        } else if (MasterController.get_digital(pros::E_CONTROLLER_DIGITAL_B)) {
+        } else if (MasterController.get_digital(pros::E_CONTROLLER_DIGITAL_B)) { //blocker down
             // Blocker down
             blockerUp.set_value(false);
-            blockerDown.set_value(false); // Set this to true for hang 
+            blockerDown.set_value(false); 
+        } else if (pros::E_CONTROLLER_DIGITAL_A) { //hang
+            blockerUp.set_value(false);
+            blockerDown.set_value(true);
         } else {
             // No blocker command, keep both solenoids off
             blockerUp.set_value(false);
             blockerDown.set_value(false);
-        }
-
-
+        } */
 
 
         // wing L
@@ -490,7 +399,7 @@ void opcontrol()
 
         // Catapult
         if (spam) {
-            catapultMotor.move(0.7 *127);
+            catapultMotor.move(127);
         } else {
         if (catacurrent == true && cataprevious == false)
         {
@@ -504,7 +413,7 @@ void opcontrol()
 
         if (abs(REST_POSITION - Rotationsensor.get_position()) > 500 || MasterController.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
         {
-            catapultMotor.move(0.7 * 127);
+            catapultMotor.move(127);
         }
         else
         {
